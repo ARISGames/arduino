3b4f4dceab6dd7008caf231262286e5ef5786ad8
Brian Theil <brthiel@wisc.edu>
2012-03-07 20:35:42 +0000

    Made a much simpler example of connecting to a server and showing progress via the multiColored LED...uses phil's library

--- arduinoMultiColorLights/arduinoMultiColorLights.ino
+++ arduinoMultiColorLights/arduinoMultiColorLights.ino
@@ -1,3 +1,5 @@
+#include <MultiLight.h>
+
 
 #include <SPI.h>
 #include <Ethernet.h>
@@ -5,87 +7,61 @@
 #include <PusherClient.h>
 
 IPAddress server(50,57,138,30); // ARIS Server
-String socket_id = "socket_id";
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0x49, 0xDE };
-byte ip[] = { 192,168,2,7 };
-byte gateway[] = { 192,168,2,1 };
+byte ip[] = { 192, 168, 2, 2 };
+byte gateway[] = { 192, 168, 2, 1 };
 byte subnet[] = { 255, 255, 255, 0 };
 
-int inputPin = 3;
-int ledPin = 2;
+MultiLight multiLight = MultiLight();
 
-const int RED_LED_PIN = 9;
-const int GREEN_LED_PIN = 10;
-const int BLUE_LED_PIN = 11;
-
-int redIntensity = 0;
-int greenIntensity = 0;
-int blueIntensity = 0;
-
-const int DISPLAY_TIME = 100; //millis
 EthernetClient arisClient;
-PusherClient client;
 
 void setup() {
-  pinMode(inputPin, INPUT);
-    pinMode(ledPin, OUTPUT);
-Serial.begin(9600);
-
+  
+  Serial.begin(9600);
+ 
+  multiLight.setPins(5,6,7);  //Set up pins 
 
   ethernetConnecting();
-
-    delay(3000);
-    
-  ethernetSucceeded();
   
-      delay(3000);
-      
-      ethernetFailed();
-
-
+  Ethernet.begin(mac,ip);
+ 
+  delay(1000); //Give time to initialize before connecting
+  
+  if(arisClient.connect(server,80)){
+     ethernetSucceeded();
+    }
+  else{
+     ethernetFailed();
+    }
 
 }
 
 void loop() {
- 
- if(digitalRead(inputPin) == HIGH){
-  digitalWrite(ledPin, LOW);
- }
- else{
-   digitalWrite(ledPin, HIGH);
-  // client.triggerEvent("aris","client-test", "{\"name\": \"Joe\", \"message_count\": 23}");
-   Serial.println("Button Pressed");
- }
 
 }
+
 void ethernetConnecting(){
   Serial.println("Ethernet Connecting");
 
-
-    analogWrite(GREEN_LED_PIN, 18);
-    analogWrite(BLUE_LED_PIN, 0);
-    analogWrite(RED_LED_PIN, 40);
-
+  multiLight.setColor(multiLight.COLOR_YELLOW);
  
 }
+
 void ethernetFailed(){
   Serial.println("Ethernet Failed");
     
- 
-    analogWrite(GREEN_LED_PIN, 0);
-    analogWrite(BLUE_LED_PIN, 0);
-    analogWrite(RED_LED_PIN, 20);
+   multiLight.setColor(multiLight.COLOR_RED);
+
 
 }
 
 void ethernetSucceeded(){
   Serial.println("Ethernet Succeeded");
 
-      analogWrite(GREEN_LED_PIN, 20);
-    analogWrite(BLUE_LED_PIN, 0);
-    analogWrite(RED_LED_PIN, 0);
+  multiLight.setColor(multiLight.COLOR_GREEN);
+
     
-  
 }
 
 
